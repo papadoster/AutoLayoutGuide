8310b9c59bc903e209541326b08513d441c25fcf
Aleksandr Karpov <papadoster13@gmail.com>
2023-01-09 21:36:21 +0100

    start

--- AutoLayoutGuide/ViewController.swift
+++ AutoLayoutGuide/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let view1: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        view.backgroundColor = .green
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(view1)
+        createView1Constraint()
     }
-
-
+    
+    func createView1Constraint() {
+        NSLayoutConstraint(item: view1,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: view, attribute: .leadingMargin,
+                           multiplier: 1.0, constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: view1,
+                           attribute: .trailingMargin,
+                           relatedBy: .equal,
+                           toItem: view, attribute: .trailingMargin,
+                           multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .top ,
+                           relatedBy: .equal,
+                           toItem: view, attribute: .topMargin,
+                           multiplier: 1.0, constant: 88).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: view1, attribute: .width,
+                           multiplier: 1.0, constant: 0).isActive = true
+        
+//        NSLayoutConstraint(item: view1,
+//                           attribute: .centerX,
+//                           relatedBy: .equal,
+//                           toItem: view, attribute: .centerX,
+//                           multiplier: 1.0, constant: 0).isActive = true
+//
+//        NSLayoutConstraint(item: view1,
+//                           attribute: .centerY,
+//                           relatedBy: .equal,
+//                           toItem: view, attribute: .centerX,
+//                           multiplier: 1.0, constant: 0).isActive = true
+//        NSLayoutConstraint(item: view1,
+//                           attribute: .width ,
+//                           relatedBy: .equal,
+//                           toItem: nil, attribute: .notAnAttribute,
+//                           multiplier: 1.0, constant: 200).isActive = true
+//        NSLayoutConstraint(item: view1,
+//                           attribute: .height,
+//                           relatedBy: .equal,
+//                           toItem: nil, attribute: .notAnAttribute,
+//                           multiplier: 1.0, constant: 200).isActive = true
+    }
+    
 }
 
