54644888eef8d1294a55642a1972f9e5486fbad1
Aleksandr Karpov <papadoster13@gmail.com>
2023-01-09 21:42:41 +0100

    2

--- AutoLayoutGuide/ViewController.swift
+++ AutoLayoutGuide/ViewController.swift
@@ -24,49 +24,49 @@ class ViewController: UIViewController {
     }
     
     func createView1Constraint() {
-        NSLayoutConstraint(item: view1,
-                           attribute: .leading,
-                           relatedBy: .equal,
-                           toItem: view, attribute: .leadingMargin,
-                           multiplier: 1.0, constant: 0).isActive = true
-        
-        NSLayoutConstraint(item: view1,
-                           attribute: .trailingMargin,
-                           relatedBy: .equal,
-                           toItem: view, attribute: .trailingMargin,
-                           multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: view1,
-                           attribute: .top ,
-                           relatedBy: .equal,
-                           toItem: view, attribute: .topMargin,
-                           multiplier: 1.0, constant: 88).isActive = true
-        NSLayoutConstraint(item: view1,
-                           attribute: .height,
-                           relatedBy: .equal,
-                           toItem: view1, attribute: .width,
-                           multiplier: 1.0, constant: 0).isActive = true
-        
 //        NSLayoutConstraint(item: view1,
-//                           attribute: .centerX,
+//                           attribute: .leading,
 //                           relatedBy: .equal,
-//                           toItem: view, attribute: .centerX,
+//                           toItem: view, attribute: .leadingMargin,
 //                           multiplier: 1.0, constant: 0).isActive = true
 //
 //        NSLayoutConstraint(item: view1,
-//                           attribute: .centerY,
+//                           attribute: .trailingMargin,
 //                           relatedBy: .equal,
-//                           toItem: view, attribute: .centerX,
+//                           toItem: view, attribute: .trailingMargin,
 //                           multiplier: 1.0, constant: 0).isActive = true
 //        NSLayoutConstraint(item: view1,
-//                           attribute: .width ,
+//                           attribute: .top ,
 //                           relatedBy: .equal,
-//                           toItem: nil, attribute: .notAnAttribute,
-//                           multiplier: 1.0, constant: 200).isActive = true
+//                           toItem: view, attribute: .topMargin,
+//                           multiplier: 1.0, constant: 88).isActive = true
 //        NSLayoutConstraint(item: view1,
 //                           attribute: .height,
 //                           relatedBy: .equal,
-//                           toItem: nil, attribute: .notAnAttribute,
-//                           multiplier: 1.0, constant: 200).isActive = true
+//                           toItem: view1, attribute: .width,
+//                           multiplier: 1.0, constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: view1,
+                           attribute: .centerX,
+                           relatedBy: .equal,
+                           toItem: view, attribute: .centerX,
+                           multiplier: 1.0, constant: 0).isActive = true
+
+        NSLayoutConstraint(item: view1,
+                           attribute: .centerY,
+                           relatedBy: .equal,
+                           toItem: view, attribute: .centerY,
+                           multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .width ,
+                           relatedBy: .equal,
+                           toItem: nil, attribute: .notAnAttribute,
+                           multiplier: 1.0, constant: 200).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: nil, attribute: .notAnAttribute,
+                           multiplier: 1.0, constant: 200).isActive = true
     }
     
 }
